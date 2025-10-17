14752efdcc1011a7298898172e1aeb60aea0fb39
ashutosh <ashutosh2013official@gmail.com>
2025-10-17 11:04:57 +0530

    draft version 3.

--- src/data/siteinfo.ts
+++ src/data/siteinfo.ts
@@ -2,13 +2,13 @@ export const siteInfo = {
   name: "Technova24x7",
   tagline: "Innovative Software Solutions",
   email: "technova24x7@gmail.com",
-  phone: "+91 81680 92721",
+  phone: "+91 9416199025",
   address: "Near sunder hospital sector 7 kurukshetra",
-  whatsapp: "+91 81680 92721",
+  whatsapp: "+91 9416199025",
   website: "https://technove24.com",
   social: {
     facebook: "https://facebook.com/technova24x7",
-    instagram: "https://instagram.com/technova24x7",
+    instagram: "https://www.instagram.com/technove24x7",
     linkedin: "https://linkedin.com/company/technova24x7"
   }
 };
